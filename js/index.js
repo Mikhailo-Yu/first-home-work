c2171345910262d80d5f63c0666e76045cb2e345
Mikhailo.Yu <miha.jewish@gmail.com>
2022-12-29 19:09:20 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//Sum
+let firstNumber = prompt ('Enter first number');
+let secondNumber = prompt ('Enter second number');
+
+alert(
+    (firstNumber + '+' + secondNumber + '=' + (Number(firstNumber) + Number(secondNumber))) + '\n' +
+    (firstNumber + '-' + secondNumber + '=' + (firstNumber - secondNumber)) + '\n' +
+    (firstNumber + '*' + secondNumber + '=' + (firstNumber * secondNumber)) + '\n' +
+    (firstNumber + '/' + secondNumber + '=' + (firstNumber / secondNumber)));
+
